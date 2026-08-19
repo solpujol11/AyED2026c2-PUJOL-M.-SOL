7696f0e45c039227b4f478343b296deb541c7b74
Maria Sol <solpujol11@gmail.com>
2026-08-19 09:37:33 -0300

    Guia2

---
+++ TrabajoPractico_1/proyecto_1/modules/guia2.py
@@ -0,0 +1,10 @@
+def calcular_potencia_dos_iterativo(n):
+    if not isinstance(n, int):
+        raise TypeError("El n debe ser un número entero")
+    if n < 0:
+        raise ValueError("El n debe ser un número entero no negativo")
+    if n == 0:
+        return 1
+    for i in range (0,n,1):
+        resultado=resultado*2
+    return resultado
