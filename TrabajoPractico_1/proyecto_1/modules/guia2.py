510403bb4d82cdc64899bc859615d30a8f473a20
Maria Sol <solpujol11@gmail.com>
2026-08-19 09:45:16 -0300

    GUIA2

--- TrabajoPractico_1/proyecto_1/modules/guia2.py
+++ TrabajoPractico_1/proyecto_1/modules/guia2.py
@@ -3,8 +3,7 @@ def calcular_potencia_dos_iterativo(n):
         raise TypeError("El n debe ser un número entero")
     if n < 0:
         raise ValueError("El n debe ser un número entero no negativo")
-    if n == 0:
-        return 1
+    resultado = 1
     for i in range (0,n,1):
         resultado=resultado*2
     return resultado
--- TrabajoPractico_1/proyecto_1/tests/test_guia2.py
+++ TrabajoPractico_1/proyecto_1/tests/test_guia2.py
@@ -1,1 +1,9 @@
-h
+from modules.guia2 import calcular_potencia_dos_iterativo
+import unittest
+
+class Test_CalculadorPotenciaDosIterativo(unittest.TestCase):
+    def test_calculo(self):
+        n = 9
+        self.assertEqual(calcular_potencia_dos_iterativo(n), 512)
+if __name__ == "__main__":
+    unittest.main()
